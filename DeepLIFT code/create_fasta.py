8c4c00ee686da065a31f6e63f6240af74d7eee60
kaixuan Deng <59245745+kaixuanDeng95@users.noreply.github.com>
2022-03-19 20:51:32 +0800

    Add files via upload

--- DeepLIFT code/create_fasta.py
+++ DeepLIFT code/create_fasta.py
@@ -1,15 +1,17 @@
 import numpy as np
 import pandas as pd
 
+data_path = './CNN_leaf_total.xlsx'
 index_path = './selected_index.txt'
 file_path = './selected_fasta.fa'
 
 if __name__ == "__main__":
     index = np.loadtxt(index_path).astype(np.int32)
-    seq = pd.read_csv('./CNN_test_leaf.tsv', sep = '\t', header = 0,usecols = [3]).values
-    seq = seq.flatten()
-    selected_seq = seq[index]
-    print(selected_seq)
+    seq = pd.read_excel(data_path, header = 0,usecols = [1,2]).values
+    
+    seq,label = seq[:,1].flatten(),seq[:,0].flatten()
+    selected_seq,selected_label = seq[index],label[index]
     with open(file_path,'a') as file:
         for i in range(selected_seq.shape[0]):
+            file.write('>'+selected_label[i]+'\n')
             file.write(selected_seq[i]+'\n')
